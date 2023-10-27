60bfd93156908f998dd95138f66f5fa1785df7fb
Mohammed Ammar <imohammedomz@gmail.com>
2023-10-27 21:06:31 +0300

    JS 4&5 Exercises

--- MyWebsite/JavaScript/Script.js
+++ MyWebsite/JavaScript/Script.js
@@ -1,65 +1,49 @@
-//        <article>
-//          <h2>Advocating for Data Privacy and Greater Security in Health Information Systems</h1>
-//          <label id="articles">Article</label>
-//          <label id="date">9/29/2023</label>
-//            <p>This report discusses the significance of greater data privacy and security in the healthcare industry. It is highlighting its impact on patients and industry outcomes while still striking a delicate balance between safeguarding data privacy and promoting medical research. <br></p>
-//            <a href="MyArticles.html">Read More</a>
-//        </article>
-
-Posts =[
-    {
-        title: "Advocating for Data Privacy and Greater Security in Health Information Systems",
-        kind : "Article",
-        kindId: "articles",
-        Date: "9/29/2023",
-        intro:"This report discusses the significance of greater data privacy and security in the healthcare industry. It is highlighting its impact on patients and industry outcomes while still striking a delicate balance between safeguarding data privacy and promoting medical research.",
-        link: "MyArticles.html"
-    },
-    {
-        title: "Devil May Cry 5...",
-        kind : "Game Review",
-        kindId: "GameReviwes",
-        Date: "10/27/2023",
-        intro:"coming soon...",
-        link: "MyArticles.html"  
+class posts{
+    constructor(title,kind,id,date,intro,link){
+        this.title = title;
+        this.kind = kind;
+        this.id = id;
+        this.date = date;
+        this.intro = intro;
+        this.link = link;
     }
-]
 
-
-let main = document.querySelector("#posts")
-for (let i = 0; i < 2; i++){
-    let art = document.createElement("article")
-    let title = document.createElement("h2")
-    //add title 
-    art.appendChild(title)
-    title.innerText = Posts[i].title
-    //add type
-    let type = document.createElement("label")
-    type.innerText = Posts[i].kind
-    type.id = Posts[i].kindId
-    //add date
-    let date = document.createElement("label")
-    date.innerText = Posts[i].Date
-    date.id = "date"
-    //add intro
-    let intro = document.createElement("p")
-    intro.innerText = Posts[i].intro
-    //add link 
-    let link = document.createElement("a")
-    link.innerText = "Read More"
-    link.href = Posts[i].link
-    //append everthing 
-    art.appendChild(title)
-    art.appendChild(type)
-    art.appendChild(date)
-    art.appendChild(intro)
-    art.appendChild(link)
-    main.appendChild(art)
-
+    loadPost(){
+        let main = document.querySelector("#posts")
+        let art = document.createElement("article")
+        let title = document.createElement("h2")
+        title.innerText = this.title
+        let type = document.createElement("label")
+        type.innerText = this.kind
+        type.id = this.id
+        let date = document.createElement("label")
+        date.innerText = this.date
+        date.id = "date"
+        let intro = document.createElement("p")
+        intro.innerText = this.intro
+        //add link 
+        let link = document.createElement("a")
+        link.innerText = "Read More"
+        link.href = this.link
+        art.appendChild(title)
+        art.appendChild(type)
+        art.appendChild(date)
+        art.appendChild(intro)
+        art.appendChild(link)
+        main.appendChild(art)
+    }
 }
 
-async function loadfact() {
-    res = await fetch("https://animechan.xyz/api/random")
+let report = new posts("Advocating for Data Privacy and Greater Security in Health Information Systems","Article","articles","9/29/2023","This report discusses the significance of greater data privacy and security in the healthcare industry. It is highlighting its impact on patients and industry outcomes while still striking a delicate balance between safeguarding data privacy and promoting medical research.","MyArticles.html")
+let DMC5 = new posts("Devil May Cry 5...","Game Review","GameReviwes","10/27/2023","coming soon...","MyArticles.html")
+let HunterXHunter = new posts("Hunter x Hunter","Anime Review","AnimeReviwes","11/7/2023","coming soon...","MyArticles.html")
+report.loadPost()
+DMC5.loadPost()
+HunterXHunter.loadPost()
+
+
+async function loadquote() {
+    res = await fetch("https://animechan.xyz/api/random/anime?title=hunter x hunter")
     json = await res.json()
     console.log(json)
     let AnimeMain = document.querySelector("#Anime")
@@ -82,6 +66,4 @@ async function loadfact() {
     AnimeMain.appendChild(art)
   }
 
-for (let i = 0; i < 3; i++){
-    loadfact(); 
-}
+loadquote();
